0d1cfc51839c03bcda2c98f73fc38bcf42eeb7fe
surbhi <sur201091@gmail.com>
2017-06-13 07:46:36 +0000

    sur

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
 var app = express();
 app.use(bodyParser.json());
-mongoose.connect('mongodb://localhost:27017/mittens');
+app.use(express.static('public')); 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/mittens');
+var Meow = mongoose.model('Meow', {
+ text: String
+  });
+app.get('/meows',function(req,res,next){
+  Meow.find({},function(err,meows){
+    return res.json(meows);
+  });
 });
+
+app.post('/meows', function(req, res, next){	
+		var newmeow = new Meow({
+			text: req.body.newmeow		
+		});
+		newmeow.save(function(err){
+			return res.send("Added Successfully");
+	});	
+	});
+
+
+
+app.listen(4000, function () {
+  console.log('Example app listening on port 4000!');
+});
+
 
